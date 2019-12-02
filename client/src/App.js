f8450c785581d42ef1d3ecfdb00120f11d9f8eb0
hollandjb <hollandjb@vcu.edu>
2019-12-01 20:00:47 -0500

    jsx fix

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,15 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Fib Calculator </h1>{" "}
-          <Link to="/"> Home </Link> <Link to="/otherpage"> Other Page </Link>{" "}
-        </header>{" "}
+          <h1 className="App-title">Fib Calculator</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage"> Other Page</Link>
+        </header>
         <div>
-          <Route exact path="/" component={Fib}>
-            {" "}
-          </Route>{" "}
-          <Route path="/otherpage" component={OtherPage}>
-            {" "}
-          </Route>{" "}
-        </div>{" "}
-      </div>{" "}
+          <Route exact path="/" component={Fib}></Route>
+          <Route path="/otherpage" component={OtherPage}></Route>
+        </div>
+      </div>
     </Router>
   );
 }
